e67019d688ab965d8f9f38280f3806f896b26742
Gerrit <gerry@DESKTOP-JDQGGBH.localdomain>
2021-03-29 23:29:05 +0200

    edit collectables

---
+++ Assets/CollectableHandler.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableHandler : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particles;
+    private Animator _animator;
+    public bool destroy;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!destroy) return;
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        var instance = Instantiate(_particles, transform.position, Quaternion.identity);
+        instance.transform.eulerAngles = Vector3.left * 90f;
+        _animator.SetBool("destroy", true);
+    }
+}
