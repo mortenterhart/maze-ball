e015a7bfb97ca86a91da920c270e8f3ffa244556
JannisStraus <81247566+JannisStraus@users.noreply.github.com>
2021-04-06 13:50:03 +0200

    Door and Button

--- Assets/TestJannis/Scripts/Board.cs
+++ Assets/TestJannis/Scripts/Board.cs
@@ -21,6 +21,7 @@ public class Board : MonoBehaviour
     void FixedUpdate()
     {
         // Edit->Project Settings->Input Manager->Axis->Horizontal/Vertical->Snap deaktivieren fuer fluessige Bewegung
+        //rb.rotation = Quaternion.Euler(angle * Input.GetAxis("Vertical"), 0f, -angle * Input.GetAxis("Horizontal"));
         rb.rotation = Quaternion.Euler(angle * Input.GetAxis("Vertical"), 0f, -angle * Input.GetAxis("Horizontal"));
     }
 
--- Assets/TestJannis/Scripts/Player.cs
+++ Assets/TestJannis/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] GameObject spawn;
+
     Rigidbody rb;
 
     void Awake()
@@ -18,22 +20,32 @@ public class Player : MonoBehaviour
 
     void Respawn()
     {
-        rb.position = new Vector3(-4f, 0.75f, -4f);
+        rb.position = spawn.transform.position;
         rb.rotation = Quaternion.identity;
         rb.velocity = Vector3.zero;
     }
 
-    void Jump()
+    /*void Jump()
     {
         rb.AddForce(5f * Vector3.up, ForceMode.Impulse);
-    }
+    }*/
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Hole"))
             Respawn();
-        else if (other.CompareTag("Bounce"))
-            Jump();
+        else if (other.CompareTag("Goal"))
+            Respawn();
+        else if (other.CompareTag("Trigger"))
+            other.GetComponent<Trigger>().Enter();
+        /*else if (other.CompareTag("Bounce"))
+            Jump();*/
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Trigger"))
+            other.GetComponent<Trigger>().Exit();
     }
 
     void Update()
