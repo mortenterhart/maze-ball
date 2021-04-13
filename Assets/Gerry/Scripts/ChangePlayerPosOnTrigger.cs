8e4c97d31fc3348f3a624debc9a2b98d35699e43
JannisStraus <81247566+JannisStraus@users.noreply.github.com>
2021-04-13 18:36:27 +0200

    Lvl 2 angefangen und TeleportHandler veraendert

--- Assets/Gerry/Scripts/ChangePlayerPosOnTrigger.cs
+++ Assets/Gerry/Scripts/ChangePlayerPosOnTrigger.cs
@@ -13,7 +13,7 @@ namespace Gerry.Scripts
             // Reset speed
             var rb = other.gameObject.GetComponent<Rigidbody>();
             rb.velocity = Vector3.zero;
-            rb.rotation = Quaternion.Euler(Vector3.zero);
+            rb.rotation = Quaternion.identity;
             rb.angularVelocity = Vector3.zero;
         
             other.gameObject.transform.position = resetPos;
--- Assets/Gerry/Scripts/TeleportHandler.cs
+++ Assets/Gerry/Scripts/TeleportHandler.cs
@@ -7,6 +7,13 @@ public class TeleportHandler : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
+
+        // Reset speed
+        var rb = other.gameObject.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.rotation = Quaternion.identity;
+        rb.angularVelocity = Vector3.zero;
+
         other.gameObject.transform.position = targetPos;
     }
 }
