d275c1eb9406901f99c00fb60b909241d2def51b
Gerrit <thegarylp@gmail.com>
2021-04-17 13:09:13 +0200

    add game end screen + include all levels in build

---
+++ Assets/Common/Scripts/Game/MainMenuHandler.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Common.Scripts.Game
+{
+    public class MainMenuHandler : MonoBehaviour
+    {
+        [SerializeField] private int level1SceneIndex;
+        [SerializeField] private int mainMenuSceneIndex;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+    
+        void Update()
+        {
+        
+        }
+
+        public void OnNewGame()
+        {
+            PlayerPrefs.SetInt("levelSave", 0);
+            SceneManager.LoadScene(level1SceneIndex);
+        }
+
+        public void OnContinue()
+        {
+            SceneManager.LoadScene(PlayerPrefs.GetInt("levelSave"));
+        }
+
+        public void OnBackToMainMenu()
+        {
+            SceneManager.LoadScene(mainMenuSceneIndex);
+        }
+    }
+}
